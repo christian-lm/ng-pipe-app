1017769c0c4223a03d618635871d6c6737c467e7
Christian Lozano <christian.lozano@neoris.com>
2022-10-16 12:55:20 +0200

    - Se crean los componentes y directorios de los componentes 'paginas' - Se implementa el mapeo de rutas

---
+++ src/app/ventas/pages/basicos/basicos.component.ts
@@ -0,0 +1,15 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-basicos',
+  templateUrl: './basicos.component.html'
+})
+export class BasicosComponent implements OnInit {
+
+  constructor() {
+  }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/ventas/pages/no-comunes/no-comunes.component.ts
@@ -0,0 +1,15 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-no-comunes',
+  templateUrl: './no-comunes.component.html'
+})
+export class NoComunesComponent implements OnInit {
+
+  constructor() {
+  }
+
+  ngOnInit(): void {
+  }
+
+}
