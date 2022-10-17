5eed71bc31fb1934e8c7baa0b505a7c9d9149cea
Christian Lozano <christian.lozano@neoris.com>
2022-10-17 17:18:24 +0200

    - Se añaden botones para probar la funcionalidad de los pipes i18n

--- src/app/ventas/pages/no-comunes/no-comunes.component.ts
+++ src/app/ventas/pages/no-comunes/no-comunes.component.ts
@@ -8,18 +8,38 @@ export class NoComunesComponent {
 
   // i18nSelect
   nombre: string = 'Christian';
-  genero: string = 'masculino';
+  genero: string = 'M';
   invitacionMap = {
-    'masculino': 'invitarlo',
-    'femenino': 'invitarla'
+    'M': 'invitarlo',
+    'F': 'invitarla'
   }
 
   // i18nPlural
-  clientes: string[] = ['Maria', 'Pepe', 'Juan'];
+  clientes: string[] = ['Maria', 'Pepe', 'Juan', 'Fran', 'Jaime'];
   clientesMap = {
     '=0': 'no tenemos ningun cliente esperando.',
-    '1': 'tenemos un cliente esperando.',
+    '=1': 'tenemos un cliente esperando.',
     'other': 'tenemos # clientes esperando.'
   }
 
+  /**
+   * Metodo para cambiar el nombre y el genero de la persona del ejemplo 1
+   */
+  cambiarPersona() {
+    // Se comprueba el genero actual
+    if (this.genero === 'M') {
+      this.nombre = 'Susana';
+      this.genero = 'F';
+    } else {
+      this.nombre = 'Christian';
+      this.genero = 'M';
+    }
+  }
+
+  /**
+   * Metodo para borrar un cliente del array de clientes
+   */
+  borrarCliente() {
+    this.clientes.splice(0, 1);
+  }
 }
