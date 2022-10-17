6ebd8acd2d5ff42ae7782cfcd67f43ff4fba0980
Christian Lozano <christian.lozano@neoris.com>
2022-10-17 17:06:54 +0200

    - Se utiliza el i18nPlural Pipe

--- src/app/ventas/pages/no-comunes/no-comunes.component.ts
+++ src/app/ventas/pages/no-comunes/no-comunes.component.ts
@@ -6,12 +6,20 @@ import {Component} from '@angular/core';
 })
 export class NoComunesComponent {
 
+  // i18nSelect
   nombre: string = 'Christian';
   genero: string = 'masculino';
-
   invitacionMap = {
     'masculino': 'invitarlo',
     'femenino': 'invitarla'
+  }
+
+  // i18nPlural
+  clientes: string[] = ['Maria', 'Pepe', 'Juan'];
+  clientesMap = {
+    '=0': 'no tenemos ningun cliente esperando.',
+    '1': 'tenemos un cliente esperando.',
+    'other': 'tenemos # clientes esperando.'
   }
 
 }
