a00bda4e6e58ca25912cd3bfbe595db0ba6e5bf2
Christian Lozano <christian.lozano@neoris.com>
2022-10-20 18:36:33 +0200

    - Se añaden pipes personalizados - Se añade tabla ordenada por pipe - Se añade tabla de PrimeNG con ordenado automático

--- src/app/ventas/pages/ordenar/ordenar.component.ts
+++ src/app/ventas/pages/ordenar/ordenar.component.ts
@@ -1,15 +1,43 @@
-import {Component, OnInit} from '@angular/core';
+import {Component} from '@angular/core';
+import {Color, Heroe} from "../../interfaces/ventas.interface";
 
 @Component({
   selector: 'app-ordenar',
   templateUrl: './ordenar.component.html'
 })
-export class OrdenarComponent implements OnInit {
+export class OrdenarComponent {
 
-  constructor() {
+  enMayusculas: boolean = true;
+  ordenarPor: string = 'nombre';
+
+  heroes: Heroe[] = [
+    {
+      nombre: 'Iron Man',
+      vuela: true,
+      color: Color.rojo
+    },
+    {
+      nombre: 'Spiderman',
+      vuela: false,
+      color: Color.rojo
+    },
+    {
+      nombre: 'Capitan América',
+      vuela: false,
+      color: Color.azul
+    },
+    {
+      nombre: 'Falcon',
+      vuela: true,
+      color: Color.negro
+    }
+  ]
+
+  toggleMayusculas() {
+    this.enMayusculas = !this.enMayusculas;
   }
 
-  ngOnInit(): void {
+  cambiarOrden(valor: string) {
+    this.ordenarPor = valor;
   }
-
 }
